80df0969d571f516d52a8a9edbf6ac065d43231a
Ryan <rydowd@gmail.com>
2019-09-18 15:34:10 -0400

    handled more edge cases

--- main.js
+++ main.js
@@ -1,69 +1,76 @@
 let output = document.querySelector(".output")
 let maths = ""
-let decCount = false
+let decimalCount = false
 
 function addNumToMaths(button) {
     console.log('button clicked')
-    if (button.textContent === "."){
-        if (decCount === false){
-            decCount = true
+    if (button.textContent === ".") {
+        if (decimalCount === false) {
+            decimalCount = true
+            if (maths === "" || isNaN(maths.slice(-1))) {
+            maths += "0"
+            }
         } else {
             return
         }
     }
-    maths += button.textContent
-    output.textContent = maths
+maths += button.textContent
+output.textContent = maths
 }
-function clearOutput(){
+function clearOutput() {
     maths = ""
-    decCount = false
-    randResponse = Math.floor(Math.random()*5)
-    if (randResponse < 1){
+    decimalCount = false
+    randResponse = Math.floor(Math.random() * 5)
+    if (randResponse < 1) {
         output.textContent = "Feed me some math"
-    } else if (randResponse < 2){
+    } else if (randResponse < 2) {
         output.textContent = "How about a fresh start"
-    } else if (randResponse < 3){
+    } else if (randResponse < 3) {
         output.textContent = "Yeah... nah"
     } else {
         output.textContent = ""
     }
-    
+
 }
-function addOperatorToMaths(button){
+function addOperatorToMaths(button) {
     console.log("adding operator")
     let lastChar = maths.slice(-1)
-    if (isNaN(lastChar) && lastChar!=="."){
+    if (isNaN(lastChar) && lastChar !== ".") {
         return
-    } else{
+    } else {
         maths += button.textContent
         output.textContent = maths
-        decCount = false
+        decimalCount = false
     }
 }
-function solve(){
+function solve() {
     console.log("solving")
     maths = eval(maths)
-    if (maths === Infinity){
+    if (maths === Infinity) {
         maths = ""
         output.textContent = "Why would you divide by zero?"
         return
     }
     output.textContent = maths
+    maths = maths.toString()
+    if(maths.includes(".")){
+        decimalCount = true
+    }
 }
 clear = document.querySelector(".clear")
-clear.addEventListener("click",clearOutput)
+clear.addEventListener("click", clearOutput)
 let numbers = document.getElementsByClassName("num")
-for (let i of numbers){
-    i.addEventListener('click',function(){
+for (let i of numbers) {
+    i.addEventListener('click', function () {
         addNumToMaths(i)
     })
 }
 let equals = document.querySelector(".equals")
-equals.addEventListener("click",solve)
+equals.addEventListener("click", solve)
 
 let operators = document.getElementsByClassName("operator")
-for (let i of operators){
-    i.addEventListener('click',function(){
+for (let i of operators) {
+    i.addEventListener('click', function () {
         addOperatorToMaths(i)
     })
 }
